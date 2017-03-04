cdbb5a7428139000e24fc482a9eb0b3d226356db
Felipe Elias Philipp <felipeelias@gmail.com>
2017-03-04 22:30:09 +0100

    Move Edge and Node inside BFS to avoid clash with other algorithms

--- lib/breadth_first_search.rb
+++ lib/breadth_first_search.rb
@@ -1,27 +1,59 @@
-Edge = Struct.new(:parent, :child, :capacity)
-
-Vertex = Struct.new(:id) do
-  def edges
-    @edges ||= {}
-  end
-
-  def each_edge
-    edges.each do |_, multiple|
-      multiple.each do |edge|
-        yield edge, edge.child
+module BFS
+  class Edge
+    class << self
+      def [](*args)
+        new(*args)
       end
+    end
+
+    attr_reader :parent, :child
+
+    def initialize(parent, child, attributes = {})
+      raise ArgumentError, "invalid :parent #{parent.inspect}" unless parent.is_a?(Node)
+      raise ArgumentError, "invalid :child #{child.inspect}" unless child.is_a?(Node)
+
+      @parent = parent
+      @child = child
+      @attributes = attributes
+    end
+
+    def fetch(*args)
+      @attributes.fetch(*args)
     end
   end
 
-  def link(vertex, capacity = 0)
-    edges[vertex] ||= []
-    edges[vertex] << Edge[self, vertex, capacity]
-    self
-  end
-end
+  class Node
+    class << self
+      def [](*args)
+        new(*args)
+      end
+    end
 
-class BFS
-  def self.search(start, finish, &filter)
+    attr_reader :id, :edges
+
+    def initialize(id)
+      @id = id
+      @edges = {}
+    end
+
+    def each_edge
+      edges.each do |_, multiple|
+        multiple.each do |edge|
+          yield edge, edge.child
+        end
+      end
+    end
+
+    def link(node, attributes = {})
+      raise ArgumentError, "invalid :node #{node.inspect}" unless node.is_a?(Node)
+
+      edges[node] ||= []
+      edges[node] << Edge[self, node, attributes]
+      self
+    end
+  end
+
+  def search(start, finish)
     queue   = [start]
     visited = {start => true}
     path    = {}
@@ -35,18 +67,22 @@ class BFS
         break
       end
 
-      current.each_edge do |edge, vertex|
-        next if filter && !filter.call(edge)
+      current.each_edge do |edge, node|
+        if block_given?
+          next if yield edge
+        end
 
-        if !visited[vertex]
-          visited[vertex] = true
-          path[vertex]    = current
+        if !visited[node]
+          visited[node] = true
+          path[node]    = current
 
-          queue << vertex
+          queue << node
         end
       end
     end
 
     found ? path : false
   end
+
+  module_function :search
 end
--- spec/breadth_first_search_spec.rb
+++ spec/breadth_first_search_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 require './lib/breadth_first_search'
 
-describe 'BFS' do
+describe BFS do
   it 'returns the vertices in order they were found' do
-    start  = Vertex[0]
-    finish = Vertex[1]
+    start  = BFS::Node[0]
+    finish = BFS::Node[1]
     start.link(finish)
 
     path, _ = BFS.search(start, finish)
@@ -12,8 +12,8 @@ describe 'BFS' do
   end
 
   it 'handles multiple edges to same node' do
-    start  = Vertex[0]
-    finish = Vertex[1]
+    start  = BFS::Node[0]
+    finish = BFS::Node[1]
     start.link(finish)
     start.link(finish)
 
@@ -24,9 +24,9 @@ describe 'BFS' do
   end
 
   it 'avoids loops' do
-    start  = Vertex[0]
-    mid    = Vertex[1]
-    finish = Vertex[3]
+    start  = BFS::Node[0]
+    mid    = BFS::Node[1]
+    finish = BFS::Node[3]
 
     start.link(mid)
     mid.link(mid)
@@ -36,20 +36,20 @@ describe 'BFS' do
   end
 
   it 'returns false if no node was found' do
-    start  = Vertex[0]
-    finish = Vertex[1]
+    start  = BFS::Node[0]
+    finish = BFS::Node[1]
 
     path, _ = BFS.search(start, finish)
     expect(path).to be_falsey
   end
 
   it 'lets you decide which edges should be used' do
-    start  = Vertex[0]
-    finish = Vertex[1]
-    start.link(finish, capacity = 0)
+    start  = BFS::Node[0]
+    finish = BFS::Node[1]
+    start.link(finish, capacity: 0)
 
     path, edges = BFS.search(start, finish) do |edge|
-      edge.capacity > 0
+      edge.fetch(:capacity) <= 0
     end
 
     expect(path).to be_falsey
